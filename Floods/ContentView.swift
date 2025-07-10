773bc35f8b5617cf1477a5e1fd3abee1578f227d
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-07-10 18:45:46 +0200

    Initial Commit

---
+++ Floods/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//
+// ContentView.swift
+// Floods
+//
+// Created by sinezeleuny on 10.07.2025
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Floods/FloodsApp.swift
@@ -0,0 +1,19 @@
+//
+//
+// FloodsApp.swift
+// Floods
+//
+// Created by sinezeleuny on 10.07.2025
+//
+        
+
+import SwiftUI
+
+@main
+struct FloodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
