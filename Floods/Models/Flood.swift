12ee3453ef2dd596d22b7988ad19c247521d0f01
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-07-12 01:45:04 +0200

    Changes in decoding

--- Floods/Models/Flood.swift
+++ Floods/Models/Flood.swift
@@ -13,7 +13,7 @@ struct Flood: Identifiable {
     let id: String // nwis_id
     let height: Double // gage_height
     let place: String // site_name
-    let description: String // description
+    let description: String? // description
     let latitude: Double
     let longitude: Double
     
@@ -38,16 +38,16 @@ extension Flood: Decodable {
         let rawId = try? container.decode(String.self, forKey: .id)
         let rawHeight = try? container.decode(Double.self, forKey: .height)
         let rawPlace = try? container.decode(String.self, forKey: .place)
-        let rawDescription = try? container.decode(String.self, forKey: .description)
+        let rawDescription = try? container.decode(String?.self, forKey: .description)
         let rawLatitude = try? container.decode(Double.self, forKey: .latitude)
         let rawLongitude = try? container.decode(Double.self, forKey: .longitude)
-        guard let id = rawId, let height = rawHeight, let place = rawPlace, let description = rawDescription, let latitude = rawLatitude, let longitude = rawLongitude else {
+        guard let id = rawId, let height = rawHeight, let place = rawPlace, let latitude = rawLatitude, let longitude = rawLongitude else {
             throw FloodError.missingData
         }
         self.id = id
         self.height = height
         self.place = place
-        self.description = description
+        self.description = rawDescription
         self.latitude = latitude
         self.longitude = longitude
     }
