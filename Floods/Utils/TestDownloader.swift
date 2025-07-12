b7589e5a79b09b432046672fc7ba87abfad41ce7
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-07-12 17:52:12 +0200

    Client test

--- Floods/Utils/TestDownloader.swift
+++ Floods/Utils/TestDownloader.swift
@@ -8,3 +8,12 @@
         
 
 import Foundation
+
+final class TestDownloader: HTTPDataDownloader {
+    
+    func httpData(from url: URL) async throws -> Data {
+        try await Task.sleep(for: .milliseconds(.random(in: 100...500)))
+        return testPoints
+    }
+    
+}
--- FloodsTests/FloodsTests.swift
+++ FloodsTests/FloodsTests.swift
@@ -24,7 +24,7 @@ struct FloodsTests {
     @Test func geoJSONDecoderDecodesGeoJSON() throws {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .millisecondsSince1970
-        let decoded = try decoder.decode(GeoJSON.self, from: testFloodsData)
+        let decoded = try decoder.decode(GeoJSON.self, from: testData)
         #expect(decoded.floods.count == 6)
         #expect(decoded.floods[0].id == "01581960")
     }
@@ -34,5 +34,12 @@ struct FloodsTests {
         decoder.dateDecodingStrategy = .millisecondsSince1970
         let decoded = try decoder.decode(FloodLocation.self, from: testDetail_02207055)
         #expect(decoded.latitude == 19.2189998626709)
+    }
+    
+    @Test func clientDoesFetchFloodData() async throws {
+        let downloader = TestDownloader()
+        let client = FloodClient(downloader: downloader)
+        let floods = try await client.floods
+        #expect(floods.count == 6)
     }
 }
